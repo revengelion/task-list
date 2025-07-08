b5d76c66dcae3b6f264761c5068942d207186f31
James <jun1718www@gmail.com>
2025-07-08 18:05:13 +0800

    Configure routing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function(){
+    return 'Hello World';
+})->name('hello');
+
+Route::get('/hallo', function(){
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function($name){
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function(){
+    return '404 Not Found';
+});
